cd932cb52ad83f999f4c1c62a318ee19d0a61453
Peter Naydenov <peter.naydenov@gmail.com>
2026-03-25 18:15:42 +0200

    Types build with typescript 6.0.2

---
+++ types/src/copyObject.d.ts
@@ -0,0 +1,3 @@
+export default copyObject;
+declare function copyObject(resource: any, result: any, extend: any, cb: any, breadcrumbs: any, ...args: any[]): void;
+//# sourceMappingURL=copyObject.d.ts.map
